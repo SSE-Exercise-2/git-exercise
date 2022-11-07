610fc0f5b7d5e95a28dc8c3f7f01f3205e1a39db
iatzak <112731474+iatzak@users.noreply.github.com>
2022-11-07 11:34:36 +0100

    Create function to remove duplication (#6)

--- exercise3.py
+++ exercise3.py
@@ -1,6 +1,12 @@
 from exercise1 import Vector
 from exercise2 import Point2D
 
+def in_interval(v: float, lo: float, hi: float) -> bool:
+    """
+    Check if value is in given interval, including extreme values.
+    """
+    return v>=lo and v<=hi
+
 
 class Rectangle:
     def __init__(self, lower_left: Point2D, dx: float, dy: float) -> None:
@@ -26,13 +32,13 @@ class Rectangle:
         return self.corner(3)
 
     def contains(self, point: Point2D, tolerance=0) -> bool:
-        # Task A: remove duplication by defining a function
-        #         that checks if a value is within an interval
-        #         and reuse that here.
+    # Task A: remove duplication by defining a function
+    #         that checks if a value is within an interval
+    #         and reuse that here.
         ll_px = point.x - self._lower_left.x
         ll_py = point.y - self._lower_left.y
-        return ll_px >= -tolerance and ll_px <= self._dx + tolerance \
-            and ll_py >= -tolerance and ll_py <= self._dy + tolerance
+        return in_interval(ll_px, 0 - tolerance, self._dx + tolerance) \
+            and in_interval(ll_py, 0 - tolerance, self._dy + tolerance)
 
     def _is_idx_on_upper_edge(self, i: int) -> bool:
         return i in [2, 3]
